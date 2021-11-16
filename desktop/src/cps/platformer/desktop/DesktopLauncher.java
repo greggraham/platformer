8cac7c124543c7231df597ddc872ddfba2a77cfd
Greg Graham <greg@grahamtx.net>
2021-11-16 09:33:54 -0600

    first commit

---
+++ desktop/src/cps/platformer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cps.platformer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cps.platformer.PlatformerMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PlatformerMain(), config);
+	}
+}
